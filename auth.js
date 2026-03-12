647a0b07a6f11fcd2557040d61b2cefdfe2546ae
Abdurahmon <farmonovabdurahmon7gmail.com>
2026-03-12 19:21:01 +0500

    fix: trust host in dev

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 const isNetlify = process.env.NETLIFY === "true";
+const isDev = process.env.NODE_ENV !== "production";
 const inferredAuthUrl =
   process.env.AUTH_URL || process.env.DEPLOY_PRIME_URL || process.env.DEPLOY_URL || process.env.URL;
 
@@ -13,7 +14,7 @@ if (!process.env.AUTH_URL && inferredAuthUrl) {
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub, Google],
-  trustHost: isNetlify,
+  trustHost: isNetlify || isDev,
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
